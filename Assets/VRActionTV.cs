9e6351a159236a788fda57a6f70379d95b83d076
Juan Cardona <juancard190@gmail.com>
2021-02-28 20:11:48 -0500

    Adding Interaction Tv

---
+++ Assets/VRActionTV.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VRActionTV : MonoBehaviour
+{
+    public GameObject m_TvImage;
+    public GameObject light_AreaLight;
+    public GameObject m_videoTv;
+
+    [Obsolete]
+    void OnEnable()
+    {
+        GetComponent<VRActionHarness>().OnActionTrigger += changeStateTV;
+      
+    }
+
+    [Obsolete]
+    private void changeStateTV()
+    {
+        VideoPlayer var_videoTv = m_videoTv.GetComponent<VideoPlayer>();
+        m_TvImage.active = !m_TvImage.active;
+        light_AreaLight.active = !light_AreaLight.active;
+
+        var_videoTv.SetDirectAudioMute(0, !var_videoTv.GetDirectAudioMute(0));
+    }
+
+    // Update is called once per frame
+
+}
